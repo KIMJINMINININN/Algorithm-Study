a8932d20d1b17941ce41d3828eff7b49f8c502a3
JINMINKIMIM <kjm9596@gmail.com>
2025-03-29 22:11:43 +0900

    [단계별 문제][반복문] 1~2번

---
+++ Baekjoon/2025/반복문/10950.py
@@ -0,0 +1,9 @@
+a = int(input())
+myList = []
+
+for i in range(a):
+    list1, list2 = map(int,input().split())
+    myList.append([list1, list2])
+
+for [first, last] in myList:
+    print(first+last)
